2e26ef8f01b85b639aeefb7f5bf606e52cb4c22c
taehan Lee <darkknight8608@gmail.com>
2017-02-19 17:36:38 +0900

    Array를 Map으로 변경하는 유틸 개발

---
+++ TRiT/www/js/comm/commUtil.js
@@ -0,0 +1,14 @@
+var COMM_UTIL = {
+  convertArrayToMap : function(array, key) {
+    var retval = [];
+    var keys = key.split(".");
+    array.forEach(function(value) {
+      var cur = value;
+      for(var index = 0; index < keys.length; index++) {
+        cur = cur[keys[index]];
+      }
+      retval[cur] = value;
+    });
+    return retval;
+  }
+};
