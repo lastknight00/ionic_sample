286852ef1c7a6370e686e4932c7dfa214d620fcb
taehan Lee <darkknight8608@gmail.com>
2017-02-26 13:38:07 +0900

    Server 연결 추가

--- TRiT/www/js/comm/ajaxService.js
+++ TRiT/www/js/comm/ajaxService.js
@@ -1,18 +1,15 @@
 angular.module('app.services')
 .factory('ajaxService', [function($window) {
   var CONTEXT_URL = 'http://127.0.0.1:8080';
-  var TEST_URL = 'http://192.168.43.197:8080';
   return {
     post: function(params) {
       $.ajax({
-          url : TEST_URL + params.url,
-          //url: CONTEXT_URL + params.url,
+          url : CONTEXT_URL + params.url,
           method:'POST',
           dataType:"json",
-          data : JSON.stringify(params.data),
+          data : params.data,
           success:function (data) {
-              console.log("The server returned " + data.length + " changes that occurred after " + modifiedSince);
-              params.callback(data);
+              params.callback(data.resultData.data);
           },
           error: function(model, response) {
               console.log(response.responseText);
@@ -21,14 +18,12 @@ angular.module('app.services')
     },
     get: function(params) {
       $.ajax({
-          url : TEST_URL + params.url,
-          //url: CONTEXT_URL + params.url,
+          url : CONTEXT_URL + params.url,
           method:'GET',
           dataType:"json",
-          data : JSON.stringify(params.data),
+          data : params.data,
           success:function (data) {
-              console.log("The server returned " + data.length + " changes that occurred after " + modifiedSince);
-              params.callback(data);
+              params.callback(data.resultData.data);
           },
           error: function(model, response) {
               console.log(response.responseText);
