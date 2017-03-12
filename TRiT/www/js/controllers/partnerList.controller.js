e361825b78526b712049f8e2aa2a782b401f57a3
taehan Lee <darkknight8608@gmail.com>
2017-03-12 16:22:32 +0900

    AWS 연동

--- TRiT/www/js/controllers/partnerList.controller.js
+++ TRiT/www/js/controllers/partnerList.controller.js
@@ -2,7 +2,7 @@ angular.module('app.controllers')
 .controller('partnerListCtrl', ['$scope', '$stateParams', '$state', 'ajaxService',
 function ($scope, $stateParams, $state, ajaxService) {
   var searchParam = {
-    pageSize : 2,
+    pageSize : 4,
     pageNum : 1,
     orderBy : 'bagBasicPrice',
     asc : false,
@@ -11,16 +11,17 @@ function ($scope, $stateParams, $state, ajaxService) {
   $scope.noMoreItemsAvailable = true;
 
   $scope.loadMore = function() {
+    console.log($scope.noMoreItemsAvailable)
     searchParam.pageNum++;
     ajaxService.get({
-      url : '/api/selectPartnerList',
+      url : '/api/selectpartnerlist',
       data : searchParam,
       callback:function(response){
         $scope.datas = $scope.datas.concat(response);
-        console.log($scope.datas)
-        console.log(response)
         $scope.$apply();
         $scope.$broadcast('scroll.infiniteScrollComplete');
+        console.log(searchParam.pageNum * searchParam.pageSize)
+        console.log(response.partnerListCnt)
       }
     });
   };
@@ -30,14 +31,14 @@ function ($scope, $stateParams, $state, ajaxService) {
 
     if(!target.attr('isLoaded')) {
       ajaxService.get({
-        url:'/api/selectPartnerInfo',
+        url:'/api/selectpartnerinfo',
         data:{
           partnerId : data.partnerId
         },
         callback:function(response){
           data.options = response.detail;
           $scope.$apply();
-          $scope.noMoreItemsAvailable = false;
+          $scope.noMoreItemsAvailable = searchParam.pageNum * searchParam.pageSize >= response.partnerListCnt;
         }
       });
       target.attr('isLoaded', true);
@@ -53,16 +54,20 @@ function ($scope, $stateParams, $state, ajaxService) {
     searchParam.pageNum = 1;
 
     ajaxService.get({
-      url : '/api/selectPartnerList',
+      url : '/api/selectpartnerlist',
       data : searchParam,
       callback:function(response){
-        $scope.datas = response;
+        $scope.datas = response.partnerList;
         $scope.$apply();
+        $scope.noMoreItemsAvailable = searchParam.pageNum * searchParam.pageSize >= response.partnerListCnt;
+
+        console.log(searchParam.pageNum * searchParam.pageSize)
+        console.log(response)
       }
     });
   }
   // SearchCtrl 에서 넘어온 데이터로 서버 검색 요청 (서버)
-  searchParam.IATA = $state.params.param.reqCity.IATA;
+  //searchParam.IATA = $state.params.param.reqCity.IATA;
   $scope.searchPartnerList('bagBasicPrice', false);
   // 정렬 버튼 이벤트 (Sorting)
   // 파트너 체크박스 클릭 이벤트 (선택한 데이터 partnerDetailCtrl로 전달)
