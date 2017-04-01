d6deacd514fbe3a164f91f70a6e0f78262c64bf6
taehan Lee <darkknight8608@gmail.com>
2017-04-02 01:20:31 +0900

    로그인 화면 개발

--- partner_web/src/index.js
+++ partner_web/src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import LoginForm from './LoginForm';
 
 ReactDOM.render(
-  <App />,
+  <LoginForm />,
   document.getElementById('root')
 );
